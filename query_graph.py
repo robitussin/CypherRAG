9ab6e8310d19b90c4e1a04ed0c1a2736134012cd
robitussin <elizerponiojr@gmail.com>
2025-01-06 09:35:19 +0800

    updated coqa evaluation

--- query_graph.py
+++ query_graph.py
@@ -579,7 +579,7 @@ class QueryGraph:
         chain = GraphCypherQAChain.from_llm(
             llm=self._llm,
             graph=self._graph,
-            verbose=False,
+            verbose=True,
             return_intermediate_steps=True,
             cypher_prompt=self._cypher_prompt,
             qa_prompt=self._qa_prompt,
